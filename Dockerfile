f5e85a318365ee31061cac1ca37708261a7990f8
mia <65930826+mia-mat@users.noreply.github.com>
2026-01-14 17:17:00 -0500

    potentially fixed boost message not sending

--- Dockerfile
+++ Dockerfile
@@ -21,9 +21,9 @@ WORKDIR /app
 LABEL internal-port="8080"
 
 LABEL arachne.name="Ninetales Discord Bot"
-LABEL arachne.version="1.1.8"
+LABEL arachne.version="1.1.9"
 
-LABEL ninetales.update-note="Ignoring case of discord when /link'ing"
+LABEL ninetales.update-note="Hopefully fixed boost message not sending"
 
 # Copy the built JAR from build stage
 COPY --from=build /app/target/*.jar ./app.jar
--- src/main/java/ws/mia/ninetales/discord/misc/ServerBoostListener.java
+++ src/main/java/ws/mia/ninetales/discord/misc/ServerBoostListener.java
@@ -1,31 +1,33 @@
 package ws.mia.ninetales.discord.misc;
 
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.events.guild.member.update.GuildMemberUpdateBoostTimeEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRoleAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jspecify.annotations.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import ws.mia.ninetales.EnvironmentService;
 
 @Component
 public class ServerBoostListener extends ListenerAdapter {
 
-	private final EnvironmentService environmentService;
+    private static final Logger log = LoggerFactory.getLogger(ServerBoostListener.class);
+    private final EnvironmentService environmentService;
 
-	public ServerBoostListener(EnvironmentService environmentService) {
-		this.environmentService = environmentService;
-	}
+    public ServerBoostListener(EnvironmentService environmentService) {
+        this.environmentService = environmentService;
+    }
 
-	@Override
-	public void onGuildMemberUpdateBoostTime(GuildMemberUpdateBoostTimeEvent event) {
-		if (event.getOldTimeBoosted() == null && event.getNewTimeBoosted() != null && environmentService.getServerBoostMessageChannelId() != null) {
-
-			TextChannel announceChannel = event.getGuild().getTextChannelById(environmentService.getServerBoostMessageChannelId());
-			if (announceChannel != null) {
-				announceChannel.sendMessage("<@%s> boosted the server :3".formatted(event.getUser().getId())).queue();
-			}
-
-		}
-
-	}
-
+    @Override
+    public void onGuildMemberRoleAdd(@NonNull GuildMemberRoleAddEvent event) {
+        super.onGuildMemberRoleAdd(event);
+        if (environmentService.getServerBoostMessageChannelId() != null && event.getRoles().stream().anyMatch(r -> r.getTags().isBoost())) {
+            TextChannel announceChannel = event.getGuild().getTextChannelById(environmentService.getServerBoostMessageChannelId());
+            if (announceChannel != null) {
+                announceChannel.sendMessage("<@%s> boosted the server :3".formatted(event.getUser().getId())).queue();
+            }
+            log.warn("DEBUG: {}", environmentService.getServerBoostMessageChannelId());
+        }
+    }
 }
