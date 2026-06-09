dbac8b86bd10c212db0ffb8154b8ddfd3dd89bc9
M <65930826+mm-mat@users.noreply.github.com>
2026-06-09 15:28:59 +0100

    aaaaaa

--- src/main/java/ws/mia/ninetales/discord/misc/DiscordRuntimeService.java
+++ src/main/java/ws/mia/ninetales/discord/misc/DiscordRuntimeService.java
@@ -69,7 +69,7 @@ public class DiscordRuntimeService {
 			PoseidonContainer ninetalesContainer = poseidonClient.getContainers().stream().filter(pc -> {
 				if (pc.getLabels() == null) return false;
 				if (!pc.getLabels().containsKey("github.repository")) return false;
-				return pc.getLabels().get("github.repositoryName").equals("ninetales") && pc.getLabels().get("github.branch").equals("master");
+				return "ninetales".equals(pc.getLabels().get("github.repositoryName")) && "master".equals(pc.getLabels().get("github.branch"));
 			}).findAny().orElseThrow();
 
 			msg.append("\n");
